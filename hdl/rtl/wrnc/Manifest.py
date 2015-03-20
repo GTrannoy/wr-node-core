a2b619b3e09c6016e663de353b48c5b261fcad98
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2015-03-20 12:08:31 +0100

    hdl/wrnc: refer to git submodules for IP cores instead of hdlmake remote links

--- hdl/rtl/wrnc/Manifest.py
+++ hdl/rtl/wrnc/Manifest.py
@@ -3,10 +3,8 @@ files = [	"wr_node_core.vhd",
 		"wr_node_core_with_etherbone.vhd",
 		"wb_remapper.vhd" ]
 modules = { 
-"local" : [ "cpu", "mqueue", "smem" ],
-"git" : [ 
-        "git@ohwr.org:hdl-core-lib/general-cores.git::tom-lm32-wrnode",
-        "git@ohwr.org:hdl-core-lib/etherbone-core.git::tom-wr-node",
-        "git@ohwr.org:hdl-core-lib/wr-cores.git::tom-wr-node",
-
+"local" : [ "cpu", "mqueue", "smem",
+        "../../ip_cores/general-cores",
+        "../../ip_cores/etherbone-core",
+        "../../ip_cores/wr-cores",
         ] }
