b1a1d06748d0847e5e56900d81cc4019986c04ea
ecalvo <eva.calvo.giraldo@cern.ch>
2016-11-14 11:11:43 +0100

    Added a comment to the WB .sh script

--- hdl/rtl/wr_d3s_adc/wb/build_d3s_acq_buffer_wb.sh
+++ hdl/rtl/wr_d3s_adc/wb/build_d3s_acq_buffer_wb.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 wbgen2 -V d3s_acq_buffer_wb.vhd -H record -p d3s_acq_buffer_wbgen2_pkg.vhd -K d3s_acq_buffer_wb.vh -s defines -C d3s_acq_buffer_wb.h -D d3s_acq_buffer_wb.html d3s_acq_buffer_wb.wb 
+
+echo ""
+echo "Moving WB generated files to the following locations..."
+echo ""
+
 mv -v d3s_acq_buffer_wb.vh ../../../testbench/include/.
 mv -v d3s_acq_buffer_wb.html ../doc/.
 mv -v d3s_acq_buffer_wb.vhd ../.
