7257034ce6c013bee262eb70bb988858ab69d5a3
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2017-02-09 22:04:27 +0100

    wrnc: removed submodule dependencies from the template's manifests

--- hdl/rtl/wrnc/Manifest.py
+++ hdl/rtl/wrnc/Manifest.py
@@ -3,8 +3,4 @@ files = [	"wr_node_core.vhd",
 		"wr_node_core_with_etherbone.vhd",
 		"wb_remapper.vhd" ]
 modules = { 
-"local" : [ "cpu", "mqueue", "smem",
-        "../../ip_cores/general-cores",
-        "../../ip_cores/etherbone-core",
-        "../../ip_cores/wr-cores",
-        ] }
+"local" : [ "cpu", "mqueue", "smem" ] }
--- hdl/top/spec/node_template/Manifest.py
+++ hdl/top/spec/node_template/Manifest.py
@@ -2,5 +2,4 @@ files = [ "spec_node_pkg.vhd",
 	  "spec_node_template.vhd",
 	  "spec_reset_gen.vhd" ];
 
-modules = { "local" : [ "../../../ip_cores/gn4124-core",
-		        "../../../rtl/wrnc" ] };
+modules = { "local" : [ "../../../rtl/wrnc" ] };
