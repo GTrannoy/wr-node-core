f19217989ccbf7bb02823b1f82609524b2b51713
Eva Calvo Giraldo <eva.calvo.giraldo@cern.ch>
2017-02-10 12:14:16 +0100

    Only displaced the license and description comments to make it work properly with doxygen.

--- hdl/top/svec/node_template/svec_node_template.vhd
+++ hdl/top/svec/node_template/svec_node_template.vhd
@@ -10,7 +10,25 @@
 -- Platform   : FPGA-generic
 -- Standard   : VHDL'93
 -------------------------------------------------------------------------------
-
+-- Copyright (c) 2014-2015 CERN
+-- 
+-- This source file is free software; you can redistribute it   
+-- and/or modify it under the terms of the GNU Lesser General   
+-- Public License as published by the Free Software Foundation; 
+-- either version 2.1 of the License, or (at your option) any   
+-- later version.                                               
+-- 
+-- This source is distributed in the hope that it will be       
+-- useful, but WITHOUT ANY WARRANTY; without even the implied   
+-- warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR      
+-- PURPOSE.  See the GNU Lesser General Public License for more 
+-- details.                                                     
+-- 
+-- You should have received a copy of the GNU Lesser General    
+-- Public License along with this source; if not, download it   
+-- from http://www.gnu.org/licenses/lgpl-2.1.html
+-- 
+-------------------------------------------------------------------------------
 
 library ieee;
 use ieee.STD_LOGIC_1164.all;
@@ -41,27 +59,6 @@ use unisim.vcomponents.all;
 --! Just instantiate this in the top level of your SVEC (see list_tdc_fd
 --! project), replacing the FineDelay/TDC mezzanines with any cores you want,
 --! synthesize and play!
--------------------------------------------------------------------------------
---! 
---! 
---! 
---! Copyright (c) 2014-2015 CERN
---! 
---! This source file is free software; you can redistribute it   
---! and/or modify it under the terms of the GNU Lesser General   
---! Public License as published by the Free Software Foundation; 
---! either version 2.1 of the License, or (at your option) any   
---! later version.                                               
---! 
---! This source is distributed in the hope that it will be       
---! useful, but WITHOUT ANY WARRANTY; without even the implied   
---! warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR      
---! PURPOSE.  See the GNU Lesser General Public License for more 
---! details.                                                     
---! 
---! You should have received a copy of the GNU Lesser General    
---! Public License along with this source; if not, download it   
---! from http://www.gnu.org/licenses/lgpl-2.1.html
 --! 
 -------------------------------------------------------------------------------
 entity svec_node_template is
