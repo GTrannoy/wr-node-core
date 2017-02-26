892adc863bbace99e1663c8171b00861aac0abcf
Eva Calvo Giraldo <eva.calvo.giraldo@cern.ch>
2017-02-26 10:53:51 +0100

    Modified Manifest of wrnc so that do not point to recursive ip_cores

--- hdl/rtl/wrnc/Manifest.py
+++ hdl/rtl/wrnc/Manifest.py
@@ -2,9 +2,14 @@ files = [	"wr_node_core.vhd",
 		"wr_node_pkg.vhd",
 		"wr_node_core_with_etherbone.vhd",
 		"wb_remapper.vhd" ]
+
 modules = { 
-"local" : [ "cpu", "mqueue", "smem",
-        "../../ip_cores/general-cores",
-        "../../ip_cores/etherbone-core",
-        "../../ip_cores/wr-cores",
+"local" : [ "cpu", "mqueue", "smem"
         ] }
+
+#modules = { 
+#"local" : [ "cpu", "mqueue", "smem",
+#        "../../ip_cores/general-cores",
+#        "../../ip_cores/etherbone-core",
+#        "../../ip_cores/wr-cores",
+#        ] }
