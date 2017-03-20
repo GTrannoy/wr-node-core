656231497f74ba0c48210cf3daa5a1d7109604ae
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2017-03-20 16:58:30 +0100

    sw: added missing files

---
+++ hdl/sw/common/lm32/wrnode.mk
@@ -0,0 +1,26 @@
+# and don't touch the rest unless you know what you're doing.
+CROSS_COMPILE ?= /opt/gcc-lm32/bin/lm32-elf-
+
+CC =		$(CROSS_COMPILE)gcc
+LD =		$(CROSS_COMPILE)ld
+OBJDUMP =	$(CROSS_COMPILE)objdump
+OBJCOPY =	$(CROSS_COMPILE)objcopy
+SIZE =		$(CROSS_COMPILE)size
+
+CFLAGS = -DWRNODE_RT -g -O3 -I. -I../common -I../../include -I../include/ -mmultiply-enabled -mbarrel-shift-enabled  -ffunction-sections -fdata-sections -Wl,--gc-sections 
+OBJS += ../common/lm32/wrn-crt0.o  ../common/rt-common.o  ../common/printf.o ../common/vsprintf-xint.o
+LDSCRIPT = ../common/lm32/wrnode.ld
+LDFLAGS= -ffunction-sections -fdata-sections -Wl,--gc-sections
+
+$(OUTPUT): $(LDSCRIPT) $(OBJS)
+	${CC} -o $(OUTPUT).elf -nostartfiles $(OBJS) -T $(LDSCRIPT) -lgcc -lc $(LDFLAGS)
+	${OBJCOPY} --remove-section .smem -O binary $(OUTPUT).elf $(OUTPUT).bin
+	${OBJDUMP} -S $(OUTPUT).elf  > disasm.S
+	../common/genraminit $(OUTPUT).bin > $(OUTPUT).ram
+	$(SIZE) $(OUTPUT).elf
+
+clean:
+	rm -f $(OBJS) $(OUTPUT).bin
+	
+install:
+	cp $(OUTPUT).bin /acc/local/share/firmware/list
---
+++ hdl/sw/common/urv/wrnode.mk
@@ -0,0 +1,31 @@
+# and don't touch the rest unless you know what you're doing.
+CROSS_COMPILE ?= /opt/gcc-riscv-5.2.0/bin/riscv64-unknown-elf-
+
+CC =		$(CROSS_COMPILE)gcc
+LD =		$(CROSS_COMPILE)ld
+OBJDUMP =	$(CROSS_COMPILE)objdump
+OBJCOPY =	$(CROSS_COMPILE)objcopy
+SIZE =		$(CROSS_COMPILE)size
+
+CFLAGS = -DWRNODE_RT -g -O2 -m32  -I. -I../common -I../../include -I../include/ -march=RV32IM
+OBJS += ../common/urv/crt0.o ../common/urv/irq.o ../common/urv/emulate.o ../common/rt-common.o  ../common/printf.o ../common/vsprintf-xint.o
+LDSCRIPT = ../common/urv/wrnode.ld
+
+$(OUTPUT): $(LDSCRIPT) $(OBJS)
+	${CC} -m32 -flto -g -o  $(OUTPUT).elf -nostartfiles $(OBJS) -T $(LDSCRIPT) -lgcc -lc
+	${OBJCOPY} --remove-section .smem -O binary $(OUTPUT).elf $(OUTPUT).bin
+	${OBJDUMP} -S $(OUTPUT).elf  > disasm.S
+	../common/genraminit $(OUTPUT).bin > $(OUTPUT).ram
+	$(SIZE) $(OUTPUT).elf
+
+../common/urv/emulate.o:	../common/urv/emulate.c
+	${CC} -O2 -m32 -march=RV32I -m32 -c $^ -o $@ -I.
+
+%.o:	%.S
+	${CC} -march=RV32I -m32 -c $^ -o $@
+
+clean:
+	rm -f $(OBJS) $(OUTPUT).bin
+	
+install:
+	cp $(OUTPUT).bin /acc/local/share/firmware/list
